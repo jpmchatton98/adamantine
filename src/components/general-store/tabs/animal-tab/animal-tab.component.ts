785596b8f7607a2087ea43518b19616e22e61a01
Jonathan McHatton <50591462+jpmchatton98@users.noreply.github.com>
2024-07-15 20:29:53 -0600

    Animals and pets go to their pages

--- src/components/general-store/tabs/animal-tab/animal-tab.component.ts
+++ src/components/general-store/tabs/animal-tab/animal-tab.component.ts
@@ -6,4 +6,13 @@ import { BaseTabComponent } from '../base-tab/base-tab.component';
   templateUrl: './animal-tab.component.html',
   styleUrls: ['./animal-tab.component.scss'],
 })
-export class AnimalTabComponent extends BaseTabComponent {}
+export class AnimalTabComponent extends BaseTabComponent {
+  public nameUrlEncode(name: string): string {
+    return (
+      name
+        ?.replace(/[ '"\(\)!\/:,&]/g, '-')
+        .replace(/--+/g, '-')
+        ?.toLowerCase() ?? ''
+    );
+  }
+}
